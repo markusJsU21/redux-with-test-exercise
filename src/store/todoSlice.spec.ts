15a9968ad460466cba02712b51a1da79bac394db
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-12-13 15:56:22 +0100

    tests reducer functions

--- src/store/todoSlice.spec.ts
+++ src/store/todoSlice.spec.ts
@@ -2,14 +2,34 @@ import reducer, {addTodo, removeTodo, sortTodos} from './todoSlice'
 import type {Todo} from '../TodoList'
 import { InitialState } from './todoSlice'
 
-describe('addTodo', () => {
-    const previousState = {todos: [],
-        todoText: ''
-    } as InitialState
+describe('todoReducer', () => {
+
+    it("should return the initial state", () => {
+        expect(reducer(undefined, { type: undefined })).toEqual({
+            todos: [],
+            todoText: ''
+        })
+    })
 
     it('should add a todo to the state when it receives a todo', () => {
+        const previousState = {todos: [],
+            todoText: ''
+        } as InitialState
         expect(reducer(previousState, addTodo({id: 'hjksgdf', text: 'hejsan'})))
         .toEqual({todos:[{id: 'hjksgdf', text: 'hejsan'}],
+        todoText: ''})
+    })
+
+    it('should remove a todo from state', () => {
+        const mockStateData = {todos: [
+          {
+            id: 'hjksgdf', text: 'hejsan'
+        }
+        ],
+            todoText: ''
+        } as InitialState
+        expect(reducer(mockStateData, removeTodo('hjksgdf')))
+        .toEqual({todos:[],
         todoText: ''})
     })
 })
